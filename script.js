5d56248c5c16d4cab636c133aef4581889eb035d
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-05-12 16:27:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ function rot13(encodedStr[i]);{
   // Only change code below this line
   for(int i = 0; i < encodedStr.length; i++){
 	  if(lookup[ encodedStr[i] ] === undefined){
-		  decodedArr[i] = encodedStr[i];
+		  decodedArr.push(encodedStr[i]);
 	  }
 	  else{
-		  decodedArr[i] = lookup[ encodedStr[i] ];
+		  decodedArr.push(lookup[ encodedStr[i] ]);
 	  }
   }
 
